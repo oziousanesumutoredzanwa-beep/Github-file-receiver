0921644290105b195c1372b8a0ddd2bfe4247ee9
oziousanesumutoredzanwa-beep <oziousanesumutoredzanwa@gmail.com>
2025-08-22 09:27:06 +0200

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,11 @@
+PI=3.14159
+GRAVITY=9.81
+radius=7
+height=14
+area_circle=PI*radius**2
+volume_circle=area_circle*height
+print("Area of circle:",area_circle)
+print("Volume of circle:",volume_circle)
+
+
+
